edc86a373f491987deeb37b82119c2131d2f14b3
Skrigan <sk_dm-821@mail.ru>
2023-01-24 03:00:45 +0300

    fix: run eslint

--- src/app/garage/garage.ts
+++ src/app/garage/garage.ts
@@ -1,22 +1,20 @@
 import { garage } from '../types';
 
 class Garage {
-	draw(garageData: Array<garage>) {
-		const fragment = document.createDocumentFragment();
-		const carTemplate = document.querySelector('#carTemplate') as HTMLTemplateElement;
-		console.log('carsData =', garageData)
-		garageData.forEach((car) => {
-			const carClone = carTemplate.content.cloneNode(true) as HTMLElement;
-			// console.log(carClone.querySelector('.car'));
-			(carClone.querySelector('.car__img') as SVGElement).style.fill = car.color;
-			carClone.querySelector('.car__title')!.textContent = car.name;
-			carClone.querySelector('.car')!.setAttribute('data-carId', `${car.id}`);
-			fragment.append(carClone);
-		})
-		document.querySelector('#garage-fullness')!.textContent = `${garageData.length}`;
-		document.querySelector('#garageList')!.innerHTML = '';
-		document.querySelector('#garageList')!.appendChild(fragment);
-	}
+    draw(garageData: Array<garage>) {
+        const fragment = document.createDocumentFragment();
+        const carTemplate = document.querySelector('#carTemplate') as HTMLTemplateElement;
+        garageData.forEach((car) => {
+            const carClone = carTemplate.content.cloneNode(true) as HTMLElement;
+            (carClone.querySelector('.car__img') as SVGElement).style.fill = car.color;
+            carClone.querySelector('.car__title')!.textContent = car.name;
+            carClone.querySelector('.car')!.setAttribute('data-carId', `${car.id}`);
+            fragment.append(carClone);
+        });
+        document.querySelector('#garage-fullness')!.textContent = `${garageData.length}`;
+        document.querySelector('#garageList')!.innerHTML = '';
+        document.querySelector('#garageList')!.appendChild(fragment);
+    }
 }
 
 export default Garage;
